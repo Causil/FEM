918161042eca2ac84a7409eef768f35935b2db1c
Javier Andrés Causil Martínez <jcausilmartinez@gmail.com>
2022-06-13 22:49:13 -0500

    Importe solo lo necesario y cambiel nombre de rigidezMatrix a Utils

--- Main/main.py
+++ Main/main.py
@@ -1,8 +1,9 @@
-import numpy as np
 from sys import path 
-path.append('../MatrixRigidex')
-import RigidexMatrix as rg
-coordinates = np.loadtxt(fname = '../Data2D/coordenadas_1.dat', dtype = float)
-elements = np.loadtxt(fname = '../Data2D/elementos_1.dat', dtype = float)
-A = rg.RigidexMatrix(coordinates, elements)
+path.append('../Utils')
+import utils as u
+from numpy import loadtxt 
+
+coordinates = loadtxt(fname = '../Data2D/coordenadas_1.dat', dtype = float)
+elements = loadtxt(fname = '../Data2D/elementos_1.dat', dtype = float)
+A = u.RigidexMatrix(coordinates, elements)
 print(A)
---
+++ Utils/utils.py
@@ -0,0 +1,22 @@
+from numpy import zeros, array, append, linalg, cross, ix_
+#import numpy as np
+
+def RigidexMatrix(coordinates, elements):
+    numberCoordenates = len(coordinates)
+    numberElements = len(elements)
+    A = zeros((numberCoordenates,numberCoordenates),float)
+    for element in elements:
+        indexElement = (array(element)-1).astype(int)
+        nodes =  coordinates[indexElement]
+        Matcoef =  append(nodes,[[1],[1],[1]],axis=1)
+        C = linalg.inv(Matcoef) #constantes de funciones bases
+        a = append(nodes[1]-nodes[0],0)
+        b = append(nodes[2]-nodes[0],0)
+        area = 0.5*linalg.norm(cross(a,b))
+        Grad = append(C[0:2],[[0,0,0]],axis=0)
+        Mele = area*Grad.transpose()*Grad
+        A[ix_(indexElement,indexElement)] = A[ix_(indexElement,indexElement)] + Mele
+    return A
+
+#if __name__ == "__main__":
+#	print("I prefer to be a module")
